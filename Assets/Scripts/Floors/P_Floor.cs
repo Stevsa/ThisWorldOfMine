dd2ae1d48393a11af75b8c1dc7065e8663b2fb78
Stevsa <Stevsa.sam@hotmail.co.uk>
2018-11-20 16:22:16 +0100

    Floor Parent Class
    The Parent Class For all floor classes

---
+++ Assets/Scripts/Floors/P_Floor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TWoM.InWorld
+{
+    public class P_Floor : MonoBehaviour
+    {
+        
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+    }
+}
