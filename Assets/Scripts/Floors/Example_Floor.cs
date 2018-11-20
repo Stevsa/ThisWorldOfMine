16ccc455f2287dfa708d921187ea186ff1d477d0
Stevsa <Stevsa.sam@hotmail.co.uk>
2018-11-20 16:24:49 +0100

    Example Floors
    New Class:
    Example FLoors

---
+++ Assets/Scripts/Floors/Example_Floor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TWoM.InWorld
+{
+    public class Example_Floor : P_Floor
+    {
+        
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+    }
+}
