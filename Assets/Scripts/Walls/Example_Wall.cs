241ee605dee78888f5cb02aa3851df0f4ee9d607
Stevsa <Stevsa.sam@hotmail.co.uk>
2018-11-21 15:21:43 +0100

    Wall Creation
    New Class:
    Wall Perent
    Example Wall

---
+++ Assets/Scripts/Walls/Example_Wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TWoM.InWorld
+{
+    public class Example_Wall : P_Wall
+    {
+        
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+    }
+}
