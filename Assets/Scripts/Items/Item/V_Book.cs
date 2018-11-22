32568e77d088fc945e4b997dc47dbe3f85f8d444
Stevsa <Stevsa.sam@hotmail.co.uk>
2018-11-22 13:57:12 +0100

    Cleanup of Walls and Floors, Added Items Basics
    Cleaned up all of the walls and floors tiles sets
    Added Peront class for items and added vertural items too

---
+++ Assets/Scripts/Items/Item/V_Book.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TWoM.Items
+{
+    public class V_Book : V_Item
+    {
+        public string BookName;
+        
+    }
+}
---
+++ Assets/Scripts/Items/P_Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TWoM.Items;
+
+namespace TWoM.Inworld
+{
+    public class P_Item : MonoBehaviour
+    {
+        public V_P_Item VItem;
+        
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+
+        }
+    }
+}
