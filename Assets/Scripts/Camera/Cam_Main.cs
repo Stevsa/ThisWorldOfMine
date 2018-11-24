1574180cee1858d44251003ae150befa4c729bd4
Stevsa <Stevsa.sam@hotmail.co.uk>
2018-11-24 13:54:20 +0100

    Added Player and Camera
    A Tempererary Player has been implimented and a main camera script created for the following of the layer

---
+++ Assets/Scripts/Camera/Cam_Main.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TWoM.Characters;
+
+namespace TWoM.Camera
+{
+    public class Cam_Main : MonoBehaviour
+    {
+        public GameObject Follow;
+        
+        void Start()
+        {
+
+        }
+        
+        void Update()
+        {
+            if (Follow != null)
+            {
+                transform.position = Follow.transform.position + new Vector3(0,0,-10);
+            }
+            else
+            {
+                Follow = FindObjectOfType<Player_Character>().gameObject;
+            }
+        }
+    }
+}
