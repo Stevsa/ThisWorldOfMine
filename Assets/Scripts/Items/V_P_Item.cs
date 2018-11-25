0c1e583333d831dbd8ef3c6e03e1eeaabf19ac5b
Stevsa <Stevsa.sam@hotmail.co.uk>
2018-11-25 13:56:45 +0100

    Item Creation Add
    Items are now able to be created from a Text File in Resources/Items

--- Assets/Scripts/Items/V_P_Item.cs
+++ Assets/Scripts/Items/V_P_Item.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace TWoM.Items
 {
@@ -23,17 +24,62 @@ namespace TWoM.Items
 
         public bool Enchantable;
 
+        protected string SaveString = "";
+
         public string ItemSave()
         {
+            if (SaveString.Length==0)
+            {
+                SaveString += "<V_P_Item>";
+            }
+            SaveString += "<Name ='" + name + "'>";
+            SaveString += "<Stackable ='" + Stackable.ToString() + "'>";
+            if (Portrait != null)
+            {
+                SaveString += "<Portrait ='" + AssetDatabase.GetAssetPath(Portrait).ToString() + "', ";
+            }
+            else
+            {
+                SaveString += "<Portrait ='NULL', ";
+            }
+            if (SmallSprite != null)
+            {
+                SaveString += "Small Sprite ='" + AssetDatabase.GetAssetPath(SmallSprite).ToString().ToString() + "'>";
+            }
+            else
+            {
+                SaveString += "Small Sprite ='NULL'>";
+            }
+            SaveString += "<Value ='" + Value + "'>";
+            SaveString += "<Enchantable =" + Enchantable.ToString() + ">";
 
-
-            return "";
+            return SaveString;
         }
 
-        public void LoadItem(string sItem)
+        public string LoadItem(string sItem)
         {
+            string SearchString = "Name ='";
+            name = sItem.Substring(sItem.IndexOf(SearchString) + SearchString.Length, sItem.IndexOf("'", sItem.IndexOf(SearchString) + SearchString.Length) - (sItem.IndexOf(SearchString) + SearchString.Length));
 
+            SearchString = "Stackable ='";
+            Stackable = "TRUE" == sItem.Substring(sItem.IndexOf(SearchString) + SearchString.Length, sItem.IndexOf("'", sItem.IndexOf(SearchString) + SearchString.Length) - (sItem.IndexOf(SearchString) + SearchString.Length));
+
+            SearchString = "Portrait ='";
+            string tPortrait = sItem.Substring(sItem.IndexOf(SearchString) + SearchString.Length, sItem.IndexOf("'", sItem.IndexOf(SearchString) + SearchString.Length) - (sItem.IndexOf(SearchString) + SearchString.Length));
+            Portrait = Resources.Load<Sprite>(tPortrait);
+
+            SearchString = "Small Sprite ='";
+            string tSmallSprite = sItem.Substring(sItem.IndexOf(SearchString) + SearchString.Length, sItem.IndexOf("'", sItem.IndexOf(SearchString) + SearchString.Length) - (sItem.IndexOf(SearchString) + SearchString.Length));
+            SmallSprite = Resources.Load<Sprite>(tSmallSprite);
             
+            SearchString = "Value ='";
+            Value = int.Parse(sItem.Substring(sItem.IndexOf(SearchString) + SearchString.Length, sItem.IndexOf("'", sItem.IndexOf(SearchString) + SearchString.Length) - (sItem.IndexOf(SearchString) + SearchString.Length)));
+
+            SearchString = "Enchantable ='";
+            Enchantable = "TRUE" == sItem.Substring(sItem.IndexOf(SearchString) + SearchString.Length, sItem.IndexOf("'", sItem.IndexOf(SearchString) + SearchString.Length) - (sItem.IndexOf(SearchString) + SearchString.Length));
+
+
+            return sItem;
         }
     }
 }
--- Assets/Test_Item_Saver.cs
+++ Assets/Test_Item_Saver.cs
@@ -1,19 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using System.Text;
 using TWoM.Items;
 
 public class Test_Item_Saver : MonoBehaviour
 {
-    public V_P_Item TestItem;
+    public List<V_P_Item> TestItems;
+
+    [TextArea]
     public string theText;
 
 	void Start () {
-        TextAsset text = Resources.Load("Items/Test_Items.txt") as TextAsset;
+        TextAsset Text = (TextAsset)Resources.Load("Items/Test_Items", typeof(TextAsset)) as TextAsset;
 
-        theText = text.text;
-	}
+        theText = Text.text;
+
+        //theText = TestItem.ItemSave();
+        string[] NewTexts = theText.Split("\n".ToCharArray());
+
+        for (int i = 0; i < NewTexts.Length; i++)
+        {
+            if (!NewTexts[i].Contains("//") && NewTexts[i].Contains("<"))
+            {
+                Debug.Log(NewTexts[i].Length);
+                TestItems.Add(CreateItemFromString(NewTexts[i]));
+            }
+        }
+        
+
+    }
+
+    V_P_Item CreateItemFromString(string str)
+    {
+        string SearchString = "<";
+        string tClass = str.Substring(str.IndexOf(SearchString) + SearchString.Length, str.IndexOf(">", str.IndexOf(SearchString) + SearchString.Length) - (str.IndexOf(SearchString) + SearchString.Length));
+        
+        V_P_Item NewItem = null;
+
+        switch (tClass)
+        {
+            case "V_P_Item":
+                NewItem = new V_P_Item();
+                NewItem.LoadItem(str);
+                break;
+            case "V_Item":
+                NewItem = new V_Item();
+                NewItem.LoadItem(str);
+                break;
+            case "V_Book":
+                NewItem = new V_Book();
+                NewItem.LoadItem(str);
+                break;
+            case "Clothing_Item":
+                NewItem = new Clothing_Item();
+                NewItem.LoadItem(str);
+                break;
+            case "Consumable_Item":
+                NewItem = new Consumable_Item();
+                NewItem.LoadItem(str);
+                break;
+            case "Quest_Item":
+                NewItem = new Quest_Item();
+                NewItem.LoadItem(str);
+                break;
+            case "Unique_Item":
+                NewItem = new Unique_Item();
+                NewItem.LoadItem(str);
+                break;
+            case "Weapon_Item":
+                NewItem = new Weapon_Item();
+                NewItem.LoadItem(str);
+                break;
+            default:
+                break;
+        }
+
+        return NewItem;
+    }
 	
 	void Update () {
 		
